88ec9eec6388c77665ecd84509fbcfb0e35813a5
Noman5237 <anonyman637@gmail.com>
2022-03-01 20:03:48 +0600

    created dynamic create chunk plugin

---
+++ plugins/transpiler/dynamic-create-chunk/src/plugin.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-param-reassign */
+const plugin = (babel) => {
+	const { types: t } = babel;
+
+	return {
+		name: 'create-chunk-plugin',
+		visitor: {
+			ExpressionStatement(path) {
+				const { expression } = path.node;
+				if (!expression) return;
+
+				const { callee } = expression;
+				if (!callee) return;
+				if (callee.name !== '__createChunk__') return;
+
+				const source = expression.arguments[0].value;
+				const importExpression = t.callExpression(t.import(), [t.stringLiteral(source)]);
+				const logicalExpression = t.logicalExpression('&&', t.identifier('undefined'), importExpression);
+				path.node.expression = logicalExpression;
+			},
+		},
+	};
+};
+
+module.exports = plugin;
